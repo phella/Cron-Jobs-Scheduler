7da43f5dc8630dda71fb4282448dd4bcc81f81b9
Philopateer Nabil <pnabil@microsoft.com>
2023-10-13 19:31:39 +0300

    Update gitignore.

--- Cron Scheduler/Program.cs
+++ Cron Scheduler/Program.cs
@@ -1,8 +1,12 @@
 ﻿
 using Cron_Scheduler;
 
+void printTimeEverySecond() {
+    Console.WriteLine(DateTime.Now);
+}
+
 var scheduler = new CronScheduler();
-scheduler.AddJob(1, TimeSpan.FromMilliseconds(20), TimeSpan.FromSeconds(1), ()=>{});
+scheduler.AddJob(id: 1, executionTimeEstimate: TimeSpan.FromMilliseconds(20), frequency: TimeSpan.FromSeconds(1), job: printTimeEverySecond);
 scheduler.AddJob(2, TimeSpan.FromMilliseconds(20), TimeSpan.FromSeconds(2), ()=>{});
 await Task.Delay(TimeSpan.FromSeconds(5));
 scheduler.RemoveJob(1);
